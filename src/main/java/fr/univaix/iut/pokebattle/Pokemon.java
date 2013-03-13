5635d3539e6ef30c672f7ab4b0ed29f8769457bb
youvann <measkevin@gmail.com>
2013-03-13 16:35:39 +0100

    Création de classe pokemon

---
+++ src/main/java/fr/univaix/iut/pokebattle/Pokemon.java
@@ -0,0 +1,36 @@
+package fr.univaix.iut.pokebattle;
+
+public class Pokemon {
+	String nomPoke;
+	String criPoke;
+	String ownerPoke;
+	
+	public Pokemon() {
+		// TODO Auto-generated constructor stub
+	}	
+	
+	@Override
+	public String toString() {
+		return "Pokemon [nomPoke=" + nomPoke + ", criPoke=" + criPoke
+				+ ", ownerPoke=" + ownerPoke + "]";
+	}
+
+	public String getNomPoke() {
+		return nomPoke;
+	}
+	public String getCriPoke() {
+		return criPoke;
+	}
+	public String getOwnerPoke() {
+		return ownerPoke;
+	}
+	public void setNomPoke(String nomPoke) {
+		this.nomPoke = nomPoke;
+	}
+	public void setCriPoke(String criPoke) {
+		this.criPoke = criPoke;
+	}
+	public void setOwnerPoke(String ownerPoke) {
+		this.ownerPoke = ownerPoke;
+	}
+}
