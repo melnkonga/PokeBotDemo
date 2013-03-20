e9a6704a4c2ff73f9def0d25cd717566b904c178
Change Me <change-me@example.com>
2013-03-20 14:26:40 +0100

    sa marche aussi

--- src/main/java/fr/univaix/iut/pokebattle/smartcells/PokemonAnswerOnlyOwnerCell.java
+++ src/main/java/fr/univaix/iut/pokebattle/smartcells/PokemonAnswerOnlyOwnerCell.java
@@ -10,7 +10,7 @@ public class PokemonAnswerOnlyOwnerCell {
 		Pokemon carapuce = new Pokemon("Carapuce","Skwalop Skwalop","J_Apple_Junior");
 		String owner=carapuce.getOwnerPoke();
 		
-		if (question.getText().contains("@Carapuce #attack #ecume @bulbizare1"))
+		if (question.getText().contains("#attack"))
 		{
 			if (owner != question.getScreenName())
 			{
