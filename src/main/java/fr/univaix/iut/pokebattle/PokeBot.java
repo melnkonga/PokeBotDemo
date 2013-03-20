4a84773b12db0e082df2af1ccaaef8975bf8a464
Change Me <change-me@example.com>
2013-03-20 11:37:50 +0100

    modif pokebot

--- src/main/java/fr/univaix/iut/pokebattle/PokeBot.java
+++ src/main/java/fr/univaix/iut/pokebattle/PokeBot.java
@@ -1,9 +1,6 @@
 package fr.univaix.iut.pokebattle;
 
-import fr.univaix.iut.pokebattle.smartcells.PokemonAnswerPersonCell;
-import fr.univaix.iut.pokebattle.smartcells.PokemonCatchCell;
 import fr.univaix.iut.pokebattle.smartcells.PokemonCriesCell;
-import fr.univaix.iut.pokebattle.smartcells.PokemonOwnerCell;
 
 
 public class PokeBot implements Bot {
@@ -12,7 +9,7 @@ public class PokeBot implements Bot {
      * find an answer.
      */
     final SmartCell[] smartCells = new SmartCell[]{
-            new PokemonAnswerPersonCell(),
+            new PokemonCriesCell(),
     };
 
     /**
