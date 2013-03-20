8306ae71bad705129068f51eaea8a0938ab1ceee
Raphael <raphderivas@gmail.com>
2013-03-20 09:01:48 +0100

    rename  f3 et test

---
+++ src/test/java/fr/univaix/iut/pokebattle/SmartCells/PokemonCatchCellTestCase.java
@@ -0,0 +1,23 @@
+package fr.univaix.iut.pokebattle.SmartCells;
+
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fr.univaix.iut.pokebattle.Tweet;
+
+import fr.univaix.iut.pokebattle.smartcells.PokemonCatchCell;
+
+public class PokemonCatchCellTestCase {
+
+    @Test
+    public void testPokeball()
+    {
+    	
+    	PokemonCatchCell pokeBot=new PokemonCatchCell();
+		assertEquals("@raph is my owner", pokeBot.ask(new Tweet("@raph","@carapuce Pokeball")));
+    }
+
+}
