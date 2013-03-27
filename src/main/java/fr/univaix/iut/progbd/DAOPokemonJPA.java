414bfe29f38e35b77612d594975b6d3e83091f9a
Julien <remy-julien@live.fr>
2013-03-27 16:40:33 +0100

    bd non fonctionnel

--- src/main/java/fr/univaix/iut/progbd/DAOPokemonJPA.java
+++ src/main/java/fr/univaix/iut/progbd/DAOPokemonJPA.java
@@ -1,68 +1,22 @@
 package fr.univaix.iut.progbd;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.TypedQuery;
-import java.util.List;
+import javax.persistence.Persistence;
 
-public class DAOPokemonJPA implements DAOPokemon {
+public class DAOPokemonJPA {
 
-    private EntityManager entityManager;
-
-    public DAOPokemonJPA(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-
-    @Override
-    public List<Pokemon> findByType(Type type) {
-        TypedQuery<Pokemon> query = entityManager.createNamedQuery(Pokemon.FIND_BY_TYPE, Pokemon.class);
-        query.setParameter("ftype", type);
-        return query.getResultList();
-    }
-
-    @Override
-    public boolean delete(Pokemon obj) {
-        try {
-            EntityTransaction tx = entityManager.getTransaction();
-            tx.begin();
-            entityManager.remove(obj);
-            tx.commit();
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    @Override
-    public List<Pokemon> findAll() {
-        TypedQuery<Pokemon> query = entityManager.createNamedQuery(Pokemon.FIND_ALL, Pokemon.class);
-        return query.getResultList();
-    }
-
-    @Override
-    public Pokemon getById(String id) {
-        return entityManager.find(Pokemon.class, id);
-    }
-
-    @Override
-    public Pokemon insert(Pokemon obj) {
-        EntityTransaction tx = entityManager.getTransaction();
-        tx.begin();
-        entityManager.persist(obj);
-        tx.commit();
-        return entityManager.find(Pokemon.class, obj.getName());
-    }
-
-    @Override
-    public boolean update(Pokemon obj) {
-        try {
-            EntityTransaction tx = entityManager.getTransaction();
-            tx.begin();
-            entityManager.merge(obj);
-            tx.commit();
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+    public void insert(Pokemon obj) {
+    	   EntityManagerFactory emf = Persistence.createEntityManagerFactory("PokemonDB");
+           EntityManager em = emf.createEntityManager();
+           EntityTransaction tx = em.getTransaction();
+           Pokemon carapuce = Pokemon.createPokemon("Carapuce","J_Apple_Junior");
+           tx.begin();
+           em.persist(carapuce);
+           tx.commit();
+           System.out.println(carapuce);
+           em.close();
+           emf.close();
     }
 }
--- src/main/java/fr/univaix/iut/progbd/Main.java
+++ src/main/java/fr/univaix/iut/progbd/Main.java
@@ -2,25 +2,22 @@ package fr.univaix.iut.progbd;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Initializes the Entity manager
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pokebattlePU");
-        EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
-
-        Pokemon pikachu = new Pokemon("Pikachu");
-        pikachu.setType1(Type.ELECTRIC);
-        em.persist(pikachu);
-
-        em.getTransaction().commit();
-
-        em.close();
-        emf.close();
+     	   EntityManagerFactory emf = Persistence.createEntityManagerFactory("pokebattlePU");
+            EntityManager em = emf.createEntityManager();
+            EntityTransaction tx = em.getTransaction();
+            Pokemon carapuce = Pokemon.createPokemon("Carapuce","J_Apple_Junior");
+            tx.begin();
+            em.persist(carapuce);
+            tx.commit();
+            System.out.println(carapuce);
+            em.close();
+            emf.close();
     }
 }
