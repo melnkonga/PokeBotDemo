b4d0103823540dfae15b5ebffa190cddcf54c676
mdraner <mathilde.renard@hotmail.fr>
2013-03-27 14:36:45 +0100

    F11

---
+++ src/main/java/fr/univaix/iut/pokebattle/smartcells/PokemonPertePVCell.java
@@ -0,0 +1,28 @@
+package fr.univaix.iut.pokebattle.smartcells;
+import fr.univaix.iut.pokebattle.Pokemon;
+import fr.univaix.iut.pokebattle.twitter.Tweet;
+
+
+public class PokemonPertePVCell {
+	
+	public String ask(Tweet question) {
+		String Resultat = null;
+		Pokemon carapuce = new Pokemon("Carapuce", "Skwalop Skwalop", "1nsanesuperstar");
+		String owner = carapuce.getOwnerPoke();
+		carapuce.setPV(10);
+		
+		if ((question.getScreenName()).equals("JugeViviane")) {
+			
+			if (question.getText().contains("-10pv")) {
+				carapuce.setPV(carapuce.getPV()-10);
+				if (carapuce.getPV()==0) {
+					Resultat = "#KO /cc @JugeViviane @" + owner;
+				}
+			}
+		
+		}
+		return Resultat;
+	}
+	
+
+}
