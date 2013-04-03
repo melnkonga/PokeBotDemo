161e0b552f8e9f5b2536578cc13a3d217e9ffd35
Change Me <change-me@example.com>
2013-04-03 15:55:55 +0200

    Ajout de DAO

---
+++ src/main/java/fr/univaix/iut/progbd/DAODresseur.java
@@ -0,0 +1,54 @@
+package fr.univaix.iut.progbd;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class DAODresseur 
+{
+	private EntityManager entityManager;
+
+	public DAODresseur(EntityManager entityManager) 
+	{
+		this.entityManager = entityManager;
+	}
+
+	public boolean delete(Dresseur dresseur) 
+	{
+		try 
+		{
+			EntityTransaction tx = entityManager.getTransaction();
+			tx.begin();
+			entityManager.remove(dresseur);
+			tx.commit();
+			return true;
+		} catch (Exception e) 
+		{
+			return false;
+		}
+	}
+
+	public Dresseur getById(String nom) {
+		return entityManager.find(Dresseur.class, nom);
+	}
+
+	public Dresseur insert(Dresseur dresseur) {
+		EntityTransaction tx = entityManager.getTransaction();
+		tx.begin();
+		entityManager.persist(dresseur);
+		tx.commit();
+		return entityManager.find(Dresseur.class, dresseur.getNom());
+	}
+
+	public boolean update(Dresseur dresseur) {
+		try {
+			EntityTransaction tx = entityManager.getTransaction();
+			tx.begin();
+			entityManager.merge(dresseur);
+			tx.commit();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+}
+
---
+++ src/main/java/fr/univaix/iut/progbd/DAOJuge_Combat.java
@@ -0,0 +1,55 @@
+package fr.univaix.iut.progbd;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class DAOJuge_Combat 
+{
+	private EntityManager entityManager;
+
+	public DAOJuge_Combat(EntityManager entityManager) 
+	{
+		this.entityManager = entityManager;
+	}
+
+	public boolean delete(Juge_Combat juge) 
+	{
+		try 
+		{
+			EntityTransaction tx = entityManager.getTransaction();
+			tx.begin();
+			entityManager.remove(juge);
+			tx.commit();
+			return true;
+		} catch (Exception e) 
+		{
+			return false;
+		}
+	}
+
+
+	public Dresseur insert(Juge_Combat juge) {
+		EntityTransaction tx = entityManager.getTransaction();
+		tx.begin();
+		entityManager.persist(juge);
+		tx.commit();
+		return entityManager.find(Dresseur.class, juge.getNom());
+	}
+
+	public boolean update(Juge_Combat juge) {
+		try {
+			EntityTransaction tx = entityManager.getTransaction();
+			tx.begin();
+			entityManager.merge(juge);
+			tx.commit();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	public Juge_Combat getById(String nom) {
+		return entityManager.find(Juge_Combat.class, nom);
+	}
+
+}
+
