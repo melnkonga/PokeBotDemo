86620fa0cbe17ad588836eea3e8e8e27bf6c60e9
Change Me <change-me@example.com>
2013-04-10 15:53:10 +0200

    chgmt gqon

---
+++ src/main/java/gson/DataObjectAttack.java
@@ -0,0 +1,65 @@
+package gson;
+
+public class DataObjectAttack {
+	public String getNiveau() {
+		return niveau;
+	}
+
+	public void setNiveau(String niveau) {
+		this.niveau = niveau;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPuissance() {
+		return puissance;
+	}
+
+	public void setPuissance(String puissance) {
+		this.puissance = puissance;
+	}
+
+	public String getPrecision() {
+		return precision;
+	}
+
+	public void setPrecision(String precision) {
+		this.precision = precision;
+	}
+
+	public String getPp() {
+		return pp;
+	}
+
+	public void setPp(String pp) {
+		this.pp = pp;
+	}
+
+	private String niveau;
+	private String nom;
+	private String puissance;
+	private String precision;
+	private String pp;
+
+	public DataObjectAttack(String niveau, String nom, String puissance,
+			String precision, String pp) {
+		this.niveau = niveau;
+		this.nom = nom;
+		this.puissance = puissance;
+		this.precision = precision;
+		this.pp = pp;
+	}
+
+	@Override
+	public String toString() {
+		return "DataObjectAttack{" + "niveau='" + niveau + '\'' + ", nom='"
+				+ nom + '\'' + ", puissance=" + puissance + ", precision="
+				+ precision + ", pp=" + pp + '}';
+	}
+}
---
+++ src/main/java/gson/bdgsonLoading.java
@@ -0,0 +1,77 @@
+package gson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+public class bdgsonLoading {
+
+	private static int pv = 100;
+
+	public static Object[] findCaracPokemon(String pokemon,
+			DataObjectPokemon[] tableau) {
+		ArrayList<Object> pokemonStat = new ArrayList<Object>();
+		for (int i = 0; i < tableau.length; ++i) {
+
+			if (pokemon.equals(tableau[i].getNom())) {
+				pokemonStat.add(tableau[i].getCapspe1());
+				pokemonStat.add(tableau[i].getCapspe2());
+				pokemonStat.add(tableau[i].getCaptureval());
+				pokemonStat.add(tableau[i].getCouleur());
+				pokemonStat.add(tableau[i].getEffortval());
+				pokemonStat.add(tableau[i].getEspece());
+				pokemonStat.add(tableau[i].getExpmax());
+				pokemonStat.add(tableau[i].getEffortval());
+				pokemonStat.add(tableau[i].getFmratio());
+				pokemonStat.add(tableau[i].getForme());
+				pokemonStat.add(tableau[i].getPoids());
+				pokemonStat.add(tableau[i].getTaille());
+				pokemonStat.add(tableau[i].getType1());
+				pokemonStat.add(tableau[i].getType2());
+			}
+
+		}
+		return pokemonStat.toArray();
+	}
+
+	public static String[] findAttPokemon(String pokemon,
+			DataObjectPokemon[] tableau) {
+		for (int i = 0; i < tableau.length; ++i) {
+
+			if (pokemon.equals(tableau[i].getNom())) {
+				DataObjectAttack[] tabAtt = tableau[i].getAttaques();
+				String[] tab = new String[tabAtt.length];
+
+				for (int j = 0; j < tabAtt.length; j++) {
+					tab[j] = tabAtt[j].getNom();
+
+				}
+				return tab;
+			}
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Gson gson = new Gson();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				bdgsonLoading.class.getClassLoader().getResourceAsStream(
+						"pokedex.json")));
+		DataObjectPokemon[] obj = gson.fromJson(br, DataObjectPokemon[].class);
+
+		findAttPokemon("Carapuce", obj);
+		
+
+	}
+
+	public static int getPv() {
+		return pv;
+	}
+
+	public static void setPv(int pv) {
+		bdgsonLoading.pv = pv;
+	}
+}
